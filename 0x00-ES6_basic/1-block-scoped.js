3c27023b86b6f9791741f92edee29a921222bc5f
Collins Enoseje <enosejecollins@gmail.com>
2024-09-30 16:08:53 +0100

    update blcok

--- 0x00-ES6_basic/1-block-scoped.js
+++ 0x00-ES6_basic/1-block-scoped.js
@@ -3,9 +3,10 @@ export default function taskBlock(trueOrFalse) {
   const task2 = true;
 
   if (trueOrFalse) {
-    const innerTask = true;
-    const innerTask2 = false;
-    return [innerTask, innerTask2];
+    // eslint-disable-next-line no-unused-vars
+    const task = true;
+    // eslint-disable-next-line no-unused-vars
+    const task2 = false;
   }
 
   return [task, task2];
