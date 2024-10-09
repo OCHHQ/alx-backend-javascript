1c51990b2ddd026bdc54a2ed0aea1f099a2a89dd
OCHHQ <enosejecollins@gmail.com>
2024-10-09 05:24:09 +0100

    check for error

--- 0x02-ES6_classes/5-building.js
+++ 0x02-ES6_classes/5-building.js
@@ -1,18 +1,14 @@
-class Building {
+export default class Building {
     constructor(sqft) {
-      this._sqft = sqft;
-  
-      // Check if this is an instance of a class that extends Building
       if (this.constructor !== Building) {
-        if (typeof this.evacuationWarningMessage !== 'function') {
+        if (this.evacuationWarningMessage === undefined) {
           throw new Error('Class extending Building must override evacuationWarningMessage');
         }
       }
+      this._sqft = sqft;
     }
   
     get sqft() {
       return this._sqft;
     }
   }
-  
-  export default Building;
