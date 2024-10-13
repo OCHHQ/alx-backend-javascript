4f00984a7f9494e228de2b2334a9529e1f341e3c
OCHHQ <enosejecollins@gmail.com>
2024-10-13 22:40:44 +0100

    promise

--- 0x01-ES6_promise/0-promise.js
+++ 0x01-ES6_promise/0-promise.js
@@ -1,10 +1,8 @@
-// 0-promise.js
-
 function getResponseFromAPI() {
-    return new Promise((resolve, reject) => {
-      // The function just needs to return a Promise
-      // We don't need to resolve or reject it here
-    });
-  }
-  
-  export default getResponseFromAPI;
+  return new Promise(() => {
+    // The function just needs to return a Promise
+    // We don't need to resolve or reject it here
+  });
+}
+
+export default getResponseFromAPI;
