b25061309b5ded5ae1349e6dd92e4758fd0ddbe2
OCHHQ <enosejecollins@gmail.com>
2024-10-13 16:38:07 +0100

    promise

--- 0x01-ES6_promise/0-promise.js
+++ 0x01-ES6_promise/0-promise.js
@@ -1,12 +1,9 @@
+// 0-promise.js
 function promiseFunction() {
   return new Promise((resolve) => {
     resolve('Success');
   });
 }
 
-// Call the function to use it
-promiseFunction().then((result) => {
-  console.log(result); // Logs "Success"
-}).catch((error) => {
-  console.error(error); // Handle any potential errors
-});
+// Export the function as default
+export default promiseFunction;
