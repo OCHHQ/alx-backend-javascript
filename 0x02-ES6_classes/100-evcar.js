bfc6131dc3a5ccc0e22170429ed448316643abe1
OCHHQ <enosejecollins@gmail.com>
2024-10-13 01:04:26 +0100

    restoftask

--- 0x02-ES6_classes/100-evcar.js
+++ 0x02-ES6_classes/100-evcar.js
@@ -1,16 +1,12 @@
-import Car from './10-car.js';
+import Car from './10-car';
 
 class EVCar extends Car {
   constructor(brand, motor, color, range) {
-    // Call the parent class (Car) constructor to initialize brand, motor, and color
     super(brand, motor, color);
-    // Initialize the range attribute
     this._range = range;
   }
 
-  // Override the cloneCar method
   cloneCar() {
-    // Return a new instance of the Car class with the same attributes
     return new Car(this._brand, this._motor, this._color);
   }
 }
--- 0x02-ES6_classes/7-airport.js
+++ 0x02-ES6_classes/7-airport.js
@@ -1,15 +1,14 @@
 class Airport {
-    constructor(name, code) {
-      // Store the parameters in underscore-prefixed attributes
-      this._name = name;
-      this._code = code;
-    }
-  
-    // Override the toString method
-    toString() {
-      return `[object ${this._code}]`;
-    }
+  constructor(name, code) {
+    // Store the parameters in underscore-prefixed attributes
+    this._name = name;
+    this._code = code;
   }
-  
-  export default Airport;
-  
+
+  // Override the toString method
+  toString() {
+    return `[object ${this._code}]`;
+  }
+}
+
+export default Airport;
