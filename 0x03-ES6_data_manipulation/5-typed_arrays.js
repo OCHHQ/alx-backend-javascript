325f6d0cf8848ba2fdcee78cf0aabbc77721f43d
OCHHQ <enosejecollins@gmail.com>
2024-10-18 01:17:36 +0100

    0x03. ES6 data manipulationJavaScriptES6

--- 0x03-ES6_data_manipulation/5-typed_arrays.js
+++ 0x03-ES6_data_manipulation/5-typed_arrays.js
@@ -1,19 +1,15 @@
-export default function createInt8TypedArray(length, position, value) {
-    // Create a new ArrayBuffer of the given length
-    const buffer = new ArrayBuffer(length);
-    
-    // Create a DataView to interact with the ArrayBuffer
-    const dataView = new DataView(buffer);
-  
-    // Check if the position is valid
-    if (position >= length) {
-      throw new Error('Position outside range');
-    }
-  
-    // Set the Int8 value at the specified position
-    dataView.setInt8(position, value);
-  
-    // Return the DataView
-    return dataView;
+// 5-typed_arrays.js
+
+function createInt8TypedArray(length, position, value) {
+  if (position >= length || position < 0) {
+    throw new Error('Position outside range');
   }
-  
+
+  const buffer = new ArrayBuffer(length);
+  const int8View = new Int8Array(buffer);
+
+  int8View[position] = value;
+  return new DataView(buffer);
+}
+
+export default createInt8TypedArray;
