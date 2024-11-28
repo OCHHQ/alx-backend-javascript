b2803a388633b7b519f4a5f620cc119a64d76957
Collins Enoseje <enosejecollins@gmail.com>
2024-11-28 05:39:59 +0100

    0 console

--- 0x05-Node_JS_basic/0-console.js
+++ 0x05-Node_JS_basic/0-console.js
@@ -1,5 +1,10 @@
-function displayMessage(message) {
-    console.log(message);
-  }
-  
-  module.exports = displayMessage;
+/**
+ * Displays a message on the STDOUT.
+ * @param {String} msg The message to display.
+ * @author enoseje collins <https://github.com/OCHHQ>
+ */
+const displayMessage = (msg) => {
+  console.log(msg);
+};
+
+module.exports = displayMessage;
